b330eab7679a6dd1f7cfacae4cc2c7d2d6158b59
kevinbtesar <kevinbtesar@gmail.com>
2020-08-13 04:43:25 -0500

    Added graphics to dashboard

--- clearboxlending/lib/screens/dashboard.dart
+++ clearboxlending/lib/screens/dashboard.dart
@@ -7,6 +7,9 @@ import 'package:clearboxlending/navigation/zoom_scaffold.dart';
 import 'package:clearboxlending/navigation/menu_fragment.dart';
 import 'package:clearboxlending/widgets/snack_bar.dart';
 
+import 'package:flutter_sparkline/flutter_sparkline.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
 class Dashboard extends StatefulWidget {
   //final VoidCallback signOut;
   final SharedPreferences _preferences;
@@ -64,7 +67,216 @@ class MainMenuState extends State<Dashboard>
     return preferences;
   }
 
+  final List<List<double>> charts = [
+    [2.9, 2.8, 3.4],
+    [
+      0.0,
+      0.3,
+    ]
+  ];
+
+  static final List<String> chartDropdownItems = ['All', 'Personal', 'Off'];
+  String actualDropdown = chartDropdownItems[0];
+  int actualChart = 0;
+
+  Widget _buildTile(Widget child, {Function() onTap}) {
+    return Material(
+        elevation: 14.0,
+        borderRadius: BorderRadius.circular(12.0),
+        shadowColor: Color(0x802196F3),
+        child: InkWell(
+            // Do onTap() if it isn't null, otherwise do print()
+            onTap: onTap != null
+                ? () => onTap()
+                : () {
+                    print('Not set yet');
+                  },
+            child: child));
+  }
+
   @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      builder: (context) => menuController,
+      child: ZoomScaffold(
+          menuScreen: MenuScreen(preferences: this.getPref()),
+          contentScreen: Layout(
+            contentBuilder: (cc) => Scaffold(
+                body: StaggeredGridView.count(
+              crossAxisCount: 2,
+              crossAxisSpacing: 12.0,
+              mainAxisSpacing: 12.0,
+              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              children: <Widget>[
+                _buildTile(
+                  Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text('Loans\navailable',
+                                  style: TextStyle(color: Colors.blueAccent)),
+                              Text('40',
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 24.0))
+                            ],
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text('Est days until\nnext available loan',
+                                  style: TextStyle(color: Colors.blueAccent)),
+                              Text('6',
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 24.0))
+                            ],
+                          ),
+                          Material(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.circular(24.0),
+                              child: Center(
+                                  child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Icon(Icons.timeline,
+                                    color: Colors.white, size: 30.0),
+                              )))
+                        ]),
+                  ),
+                ),
+                _buildTile(
+                  Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Material(
+                              color: Colors.teal,
+                              shape: CircleBorder(),
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Icon(Icons.settings_applications,
+                                    color: Colors.white, size: 30.0),
+                              )),
+                          Padding(padding: EdgeInsets.only(bottom: 16.0)),
+                          Text('Loan \$20',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 24.0)),
+                          Text('Get a \$20 for 1 week.',
+                              style: TextStyle(color: Colors.black45, fontSize: 12.0)),
+                        ]),
+                  ),
+                ),
+                _buildTile(
+                  Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Material(
+                              color: Colors.amber,
+                              shape: CircleBorder(),
+                              child: Padding(
+                                padding: EdgeInsets.all(16.0),
+                                child: Icon(Icons.notifications,
+                                    color: Colors.white, size: 30.0),
+                              )),
+                          Padding(padding: EdgeInsets.only(bottom: 16.0)),
+                          Text('Alerts',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 24.0)),
+                          DropdownButton(
+                              isDense: true,
+                              value: actualDropdown,
+                              onChanged: (String value) => setState(() {
+                                    actualDropdown = value;
+                                    actualChart = chartDropdownItems
+                                        .indexOf(value); // Refresh the chart
+                                  }),
+                              items: chartDropdownItems.map((String title) {
+                                return DropdownMenuItem(
+                                  value: title,
+                                  child: Text(title,
+                                      style: TextStyle(
+                                          color: Colors.blue,
+                                          fontWeight: FontWeight.w400,
+                                          fontSize: 14.0)),
+                                );
+                              }).toList())
+                        ]),
+                  ),
+                ),
+                _buildTile(
+                  Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text('Help!',
+                                  style: TextStyle(color: Colors.redAccent)),
+                              Text('How does it work?',
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 24.0))
+                            ],
+                          ),
+                          Material(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(24.0),
+                              child: Center(
+                                  child: Padding(
+                                padding: EdgeInsets.all(16.0),
+                                child: Icon(Icons.store,
+                                    color: Colors.white, size: 30.0),
+                              )))
+                        ]),
+                  ),
+                  onTap: () => {},
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    showSnackBar(
+                        context, "Yay! It\'s Kevin\'s SnackBar!", "My Undo");
+                  },
+                  child: Text('Show Kevin\'s SnackBar'),
+                ),
+              ],
+              staggeredTiles: [
+                StaggeredTile.extent(2, 110.0),
+                StaggeredTile.extent(1, 180.0),
+                StaggeredTile.extent(1, 180.0),
+                StaggeredTile.extent(2, 120.0),
+                StaggeredTile.extent(2, 110.0),
+              ],
+            )),
+          ),
+          preferences: this.getPref(),
+          title: "Dashboard"),
+    );
+  }
+
+  /*@override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       builder: (context) => menuController,
@@ -93,7 +305,7 @@ class MainMenuState extends State<Dashboard>
           preferences: this.getPref(),
           title: "Dashboard"),
     );
-  }
+  }*/
 
   //  Action on Bottom Bar Press
   void reds(selectedIndex) {
